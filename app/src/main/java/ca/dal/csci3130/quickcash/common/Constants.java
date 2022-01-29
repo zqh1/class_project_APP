1d407845265b2a3548556a8c68d23a260379a455
Luis Cabarique <ls939126@dal.ca>
2022-01-28 20:16:08 -0400

    Firebase db connected

--- app/src/main/java/ca/dal/csci3130/quickcash/common/Constants.java
+++ app/src/main/java/ca/dal/csci3130/quickcash/common/Constants.java
@@ -1,7 +1,7 @@
 package ca.dal.csci3130.quickcash.common;
 
 public class Constants {
-    public static final String FIREBASE_URL = "<Add your userdb url here>";
+    public static final String FIREBASE_URL = "https://csci3130-group-17-default-rtdb.firebaseio.com/";
     // Add all your constants here
     public final String sample = "Sample";
 }
