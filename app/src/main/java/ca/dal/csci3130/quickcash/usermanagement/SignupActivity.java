c49087ea30e7eff21b20ba5fe9b147f2bb26d442
Luis Cabarique <ls939126@dal.ca>
2022-02-06 11:46:03 -0400

    US1-US2 - ET2 -> Backend created that read and store user data from sign up screen

--- app/src/main/java/ca/dal/csci3130/quickcash/usermanagement/SignupActivity.java
+++ app/src/main/java/ca/dal/csci3130/quickcash/usermanagement/SignupActivity.java
@@ -1,12 +1,26 @@
 package ca.dal.csci3130.quickcash.usermanagement;
 
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import ca.dal.csci3130.quickcash.R;
 
 public class SignupActivity extends AppCompatActivity {
+
+    private EditText firstNameField;
+    private EditText lastNameField;
+    private EditText emailField;
+    private EditText passwordField;
+    private EditText confirmPasswordField;
+    private EditText phoneField;
+    private Button signUpBtn;
+    private Button signInBtn;
+    private Spinner userTypeSpinner;
+
+    private UserInterface user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +28,47 @@ public class SignupActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
 
         // Add logic for signup
+
+        //Link screen items
+        linkScreenItems();
+
+        //Set buttons on click listeners
+        setButtonsActions();
+    }
+
+    private void linkScreenItems() {
+
+        firstNameField = findViewById(R.id.firstNameInput);
+        lastNameField = findViewById(R.id.lastNameInput);
+        emailField = findViewById(R.id.emailInput);
+        passwordField = findViewById(R.id.passwordInput);
+        confirmPasswordField = findViewById(R.id.confirmPasswordInput);
+        phoneField = findViewById(R.id.phoneInput);
+        signUpBtn = findViewById(R.id.signUpBtn);
+        signInBtn = findViewById(R.id.signInBtn);
+        userTypeSpinner = findViewById(R.id.userTypeInput);
+    }
+
+    private void setButtonsActions() {
+
+        signUpBtn.setOnClickListener(view -> signUpBtnAction());
+    }
+
+    private void signUpBtnAction() {
+
+        readUserData();
+    }
+
+    private void readUserData() {
+
+        user = new User();
+        user.setFirstName(firstNameField.getText().toString().trim());
+        user.setLastName(lastNameField.getText().toString().trim());
+        user.setEmail(emailField.getText().toString().trim());
+        user.setPassword(passwordField.getText().toString().trim());
+        user.setConfirmPassword(confirmPasswordField.getText().toString().trim());
+        user.setPhone(phoneField.getText().toString().trim());
+        if (userTypeSpinner.getSelectedItem().toString().equals("Employee")) user.setIsEmployee("y");
+        else user.setIsEmployee("n");
     }
 }
