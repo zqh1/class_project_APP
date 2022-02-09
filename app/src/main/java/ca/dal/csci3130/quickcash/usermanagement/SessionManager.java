c9d46ef8633aecfb46408ff47e7777caa4911cb6
Tanachai Pusuk <tn213791@dal.ca>
2022-02-09 04:23:04 -0400

    Clear any update on SessionManager.java

--- app/src/main/java/ca/dal/csci3130/quickcash/usermanagement/SessionManager.java
+++ app/src/main/java/ca/dal/csci3130/quickcash/usermanagement/SessionManager.java
@@ -1,47 +1,33 @@
 package ca.dal.csci3130.quickcash.usermanagement;
 
 public class SessionManager implements SessionManagerInterface {
-
-    private String email = new String();
-    private String password = new String();
-    private String name = new String();
-    private boolean isLoggedIn = false;
-
-
     @Override
     public void createLoginSession(String email, String password, String name) {
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        isLoggedIn = true;
+
     }
 
     @Override
     public void checkLogin() {
-        if(isLoggedIn = true){
-        }
+
     }
 
     @Override
     public void logoutUser() {
-        this.email = new String();
-        this.password = new String();
-        this.name = new String();
-        this.isLoggedIn = false;
+
     }
 
     @Override
     public boolean isLoggedIn() {
-        return isLoggedIn;
+        return false;
     }
 
     @Override
     public String getKeyName() {
-        return name;
+        return null;
     }
 
     @Override
     public String getKeyEmail() {
-        return email;
+        return null;
     }
 }
