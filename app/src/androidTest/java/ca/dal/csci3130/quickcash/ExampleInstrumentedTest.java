cc19d03572c6bf6b4cd04f507e773b701b0ab0b6
Jay Pusuk <tn213791@dal.ca>
2022-02-09 04:10:00 -0400

    Pulled from main(merged with signup) and implement main activity

--- app/src/androidTest/java/ca/dal/csci3130/quickcash/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ca/dal/csci3130/quickcash/ExampleInstrumentedTest.java
@@ -2,13 +2,28 @@ package ca.dal.csci3130.quickcash;
 
 import android.content.Context;
 
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -17,10 +32,38 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> myRule = new ActivityScenarioRule<>(MainActivity.class);
+    public IntentsTestRule<MainActivity> myIntentRule = new IntentsTestRule<>(MainActivity.class);
+
+    @BeforeClass
+    public static void setup() {
+        Intents.init();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        System.gc();
+    }
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
         assertEquals("ca.dal.csci3130.quickcash", appContext.getPackageName());
+    }
+
+    @Test
+    public void checkIfDestinationIsLoginPage(){
+        onView(withId(R.id.loginButton)).perform(click());
+        onView(withId(R.id.loginBtn)).check(matches(withText("Login")));
+    }
+
+    @Test
+    public void checkIfDestinationIsSignupPage(){
+        onView(withId(R.id.signupButton)).perform(click());
+        onView(withId(R.id.signUpLabel)).check(matches(withText("Sign up")));
     }
 }
