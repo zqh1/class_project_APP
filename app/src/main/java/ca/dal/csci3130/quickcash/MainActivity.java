6f759e580846d92961b87bcf3d2094eefa75e27c
Luis Cabarique <ls939126@dal.ca>
2022-02-10 21:04:32 -0400

    Session Manager implemented but not tested

--- app/src/main/java/ca/dal/csci3130/quickcash/MainActivity.java
+++ app/src/main/java/ca/dal/csci3130/quickcash/MainActivity.java
@@ -1,15 +1,8 @@
 package ca.dal.csci3130.quickcash;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import ca.dal.csci3130.quickcash.usermanagement.LoginActivity;
-import ca.dal.csci3130.quickcash.usermanagement.SignupActivity;
-
+import ca.dal.csci3130.quickcash.usermanagement.SessionManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,22 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Add logic to handle the two buttons added in the UI
-        Button login = findViewById(R.id.loginButton);
-        Button signup = findViewById(R.id.signupButton);
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class).putExtra("Dest", "LoginPage"));
-            }
-        });
-
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, SignupActivity.class).putExtra("Dest", "SignupPage"));
-            }
-        });
+        new SessionManager(this).checkLogin();
     }
 }
--- app/src/main/java/ca/dal/csci3130/quickcash/usermanagement/SessionManager.java
+++ app/src/main/java/ca/dal/csci3130/quickcash/usermanagement/SessionManager.java
@@ -1,45 +1,55 @@
 package ca.dal.csci3130.quickcash.usermanagement;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import ca.dal.csci3130.quickcash.common.Constants;
+
 public class SessionManager implements SessionManagerInterface {
 
-    private String email = new String();
-    private String password = new String();
-    private String name = new String();
-    private boolean isLoggedIn = false;
+    Context context;
+    SharedPreferences sharePref;
+    SharedPreferences.Editor editor;
 
+    public SessionManager(Context context) {
+        this.context = context;
+        this.sharePref = context.getSharedPreferences("PREF", Context.MODE_PRIVATE);
+        this.editor = sharePref.edit();
+    }
 
     @Override
     public void createLoginSession(String email, String password, String name) {
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        isLoggedIn = true;
+        editor.putString(Constants.EMAIL_KEY, email);
+        editor.putString(Constants.PASSWORD_KEY, password);
+        editor.putString(Constants.NAME_KEY, name);
+        editor.apply();
     }
 
     @Override
     public void checkLogin() {
+        if (!isLoggedIn()) context.startActivity(new Intent(context, LoginActivity.class));
     }
 
     @Override
     public void logoutUser() {
-        this.email = new String();
-        this.password = new String();
-        this.name = new String();
-        this.isLoggedIn = false;
+        editor.clear();
+        editor.apply();
     }
 
     @Override
     public boolean isLoggedIn() {
-        return isLoggedIn;
+        return sharePref.contains(Constants.EMAIL_KEY) && sharePref.contains(Constants.NAME_KEY) &&
+                sharePref.contains(Constants.PASSWORD_KEY);
     }
 
     @Override
     public String getKeyName() {
-        return name;
+        return sharePref.getString(Constants.NAME_KEY,null);
     }
 
     @Override
     public String getKeyEmail() {
-        return email;
+        return sharePref.getString(Constants.EMAIL_KEY, null);
     }
 }
