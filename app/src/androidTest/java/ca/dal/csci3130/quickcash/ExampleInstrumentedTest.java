5f5c740679a0df0363ec9addfdf049a2175e66d7
Jay Pusuk <tn213791@dal.ca>
2022-02-11 20:00:42 -0400

    Clean ExampleInstrumentedTest.java

--- app/src/androidTest/java/ca/dal/csci3130/quickcash/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/ca/dal/csci3130/quickcash/ExampleInstrumentedTest.java
@@ -3,7 +3,6 @@ package ca.dal.csci3130.quickcash;
 import android.content.Context;
 
 import androidx.test.espresso.intent.Intents;
-import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -14,14 +13,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.intent.Intents.intended;
-import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 
 
@@ -35,7 +26,6 @@ public class ExampleInstrumentedTest {
 
     @Rule
     public ActivityScenarioRule<MainActivity> myRule = new ActivityScenarioRule<>(MainActivity.class);
-    public IntentsTestRule<MainActivity> myIntentRule = new IntentsTestRule<>(MainActivity.class);
 
     @BeforeClass
     public static void setup() {
@@ -55,15 +45,4 @@ public class ExampleInstrumentedTest {
         assertEquals("ca.dal.csci3130.quickcash", appContext.getPackageName());
     }
 
-    @Test
-    public void checkIfDestinationIsLoginPage(){
-        onView(withId(R.id.loginButton)).perform(click());
-        onView(withId(R.id.loginBtn)).check(matches(withText("Login")));
-    }
-
-    @Test
-    public void checkIfDestinationIsSignupPage(){
-        onView(withId(R.id.signupButton)).perform(click());
-        onView(withId(R.id.signUpLabel)).check(matches(withText("Sign up")));
-    }
 }
