822dcf57f1af96b269f0d0c13838569bcd13e330
Luis Cabarique <ls939126@dal.ca>
2022-02-23 10:52:49 -0400

    US12 - 0: Initial file creation for job creation and management.

---
+++ app/src/main/java/ca/dal/csci3130/quickcash/jobmanagement/JobActivity.java
@@ -0,0 +1,16 @@
+package ca.dal.csci3130.quickcash.jobmanagement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.dal.csci3130.quickcash.R;
+
+public class JobActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job);
+    }
+}
