ba6eb1a90d63b6d2834a6c80ae7ed9e447daf190
Luis Cabarique <ls939126@dal.ca>
2022-02-25 16:34:50 -0400

    US 12 - ET3: Job posting complete, Job is being saved on firebase

--- app/src/main/java/ca/dal/csci3130/quickcash/jobmanagement/JobVerification.java
+++ app/src/main/java/ca/dal/csci3130/quickcash/jobmanagement/JobVerification.java
@@ -2,19 +2,82 @@ package ca.dal.csci3130.quickcash.jobmanagement;
 
 public class JobVerification {
 
-    public boolean validTitle(String title) {
-        return false;
+    private JobInterface job;
+
+    private JobActivity activity;
+
+    public JobVerification() {
+        activity = null;
     }
 
-    public boolean validDate(int year, int month, int day, int hour, int minute) {
-        return false;
+    public JobVerification(JobActivity activity) {
+        this.activity = activity;
+    }
+
+    public void setJob(JobInterface job) {
+        this.job = job;
+    }
+
+    public boolean[] verifyFields() {
+
+        boolean[] fieldStatus = new boolean[] {true, true, true, true, true};
+        boolean allFieldCorrect = true;
+
+        if (!validTitle(job.getTitle())){
+            fieldStatus[0] = false;
+            allFieldCorrect = false;
+        }
+
+        if (!validDescription(job.getDescription())){
+            fieldStatus[1] = false;
+            allFieldCorrect = false;
+        }
+
+        if (!validDuration(job.getDuration())){
+            fieldStatus[2] = false;
+            allFieldCorrect = false;
+        }
+
+        if (!validSalary(job.getSalary())){
+            fieldStatus[3] = false;
+            allFieldCorrect = false;
+        }
+
+        if (!validTags(job.getTags())){
+            fieldStatus[4] = false;
+            allFieldCorrect = false;
+        }
+
+        if (allFieldCorrect) {
+            pushJobToFirebase();
+        }
+
+        return fieldStatus;
+    }
+
+    public boolean validTitle(String title) {
+        return !title.isEmpty() && title.length() <= 250;
+    }
+
+    public boolean validDescription(String description) {
+        return !description.isEmpty() && description.length() <= 500;
     }
 
     public boolean validDuration(int duration) {
-        return false;
+        return duration > 0 && duration <= 99;
     }
 
     public boolean validSalary(int salary) {
-        return false;
+        //Minimum NS wage 13.35$ per hour
+        return salary > 13.35 && salary <= 10000;
+    }
+
+    public boolean validTags(String tags) {
+        return !tags.isEmpty() && tags.length() <= 500;
+    }
+
+    //Private method that push job into job manager to being push onto the database
+    private void pushJobToFirebase() {
+        new JobDAO().add(job);
     }
 }
