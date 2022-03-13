39de845aff6f88e4c974789effee9e4ecce05985
amirah <am263157@dal.ca>
2022-03-12 23:59:20 -0400

    Employer homepage espresso testing

--- app/src/androidTest/java/ca/dal/csci3130/quickcash/home/EmployerHomeActivityEspressoTest.java
+++ app/src/androidTest/java/ca/dal/csci3130/quickcash/home/EmployerHomeActivityEspressoTest.java
@@ -11,6 +11,7 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
@@ -18,6 +19,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 import ca.dal.csci3130.quickcash.R;
+import ca.dal.csci3130.quickcash.jobmanagement.JobActivity;
 import ca.dal.csci3130.quickcash.testConstants;
 import ca.dal.csci3130.quickcash.usermanagement.LoginActivity;
 
@@ -65,4 +67,37 @@ public class EmployerHomeActivityEspressoTest {
 
         onView(withId(R.id.name)).check(matches(withText("")));
     }
+
+    @Test
+    public void checkCreateJobButton(){
+        onView(withId(R.id.name)).perform(typeText("employer@dal.ca"));
+        onView(withId(R.id.password)).perform(typeText("Ab12345##"));
+        onView(withId(R.id.loginBtn)).perform(click());
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.createJobBtn)).check(matches(withText("CREATE NEW JOB")));
+    }
+
+    @Test
+    public void checkCreateJobButtonRedirect(){
+        onView(withId(R.id.name)).perform(typeText("employer@dal.ca"));
+        onView(withId(R.id.password)).perform(typeText("Ab12345##"));
+        onView(withId(R.id.loginBtn)).perform(click());
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        onView(withId(R.id.createJobBtn)).perform(click());
+        intended(hasComponent(JobActivity.class.getName()));
+
+    }
+
 }
